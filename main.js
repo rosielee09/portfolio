20e8240c82ffc14a4f5f83c1cfbf70667998f015
rosie <123128268+rosielee09@users.noreply.github.com>
2023-04-04 16:13:23 -0700

    JS: Home contact btn scroll

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 'use strict';
 
-
 // navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  if(window.scrollY > navbarHeight) {
+  if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
-  }else {
+  } else {
     navbar.classList.remove('navbar--dark');
   }
 });
 
-// mavbar click scroll down to id
+// navbar click scroll down to id
 const navbarMenu = document.querySelector('.navbar__menu');
 
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
-  if(link == null) {
+  if (link == null) {
     return;
-  }  
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: 'smooth', block:'center'});
-  
-
+  }
+  scrollIntoView(link);
 });
+
+// handle click on "talk me" button on home
+const contact = document.querySelector('.home__contact');
+contact.addEventListener('click', (event) => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth'});
+}
