2fc324a69af72fe241965e25cd40ee1c8b9016b6
rosie <123128268+rosielee09@users.noreply.github.com>
2023-04-05 17:36:05 -0700

    JS: Arro up & projects animation

--- main.js
+++ main.js
@@ -29,7 +29,66 @@ contact.addEventListener('click', (event) => {
   scrollIntoView('#contact');
 });
 
+// when scroll, home&contact  fade out
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+const contactBtn = document.querySelector('.home__contact');
+document.addEventListener('scroll', () => {
+  contactBtn.style.opacity = 1 - window.scrollY / homeHeight;
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// when mouse on contact btn - recover opacity
+contactBtn.addEventListener('mouseenter', (e) => {
+  contactBtn.style.opacity = 1;
+});
+
+// when mouse off contact btn - lose opacity
+contactBtn.addEventListener('mouseleave', (e) => {
+  contactBtn.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth'});
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
+
+// When the user scrolls down 20px, show the button
+
+const upBtn = document.querySelector('#upBtn');
+document.addEventListener('scroll', () => {
+  upBtn.classList.toggle('visible', document.documentElement.scrollTop > 20);
+});
+
+// When the user clicks on the button, scroll to the top of the document
+upBtn.addEventListener('click', () => {
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+// when click butn, filter by category
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  projectContainer.classList.add('anim-out');
+      setTimeout(() => {
+       projects.forEach((project) => {
+        if (filter === '*' || filter === project.dataset.type) {
+           project.classList.remove('invisible');
+         } else {
+         project.classList.add('invisible');
+         }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+});
